f27267546a7933a66f9aac2ee0781829de18ec41
Joel Duffin <joel.duffin@gmail.com>
2009-12-12 21:26:20 -0700

    Modified indexer so it will try to restart solr on two ports

--- src/edu/usu/cosl/indexer/SolrCoreReloader.java
+++ src/edu/usu/cosl/indexer/SolrCoreReloader.java
@@ -5,12 +5,11 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.Vector;
+import edu.usu.cosl.util.Daemon;
 
-import org.apache.solr.core.SolrCore;
-
-public class SolrCoreReloader extends edu.usu.cosl.util.Daemon {
-	private static void askSolrToReloadCore(String sCore) throws Exception {
-		final String sReloadCommand = "http://localhost:8982/solr/admin/cores?action=RELOAD&core=";
+public class SolrCoreReloader extends Daemon {
+	private static void askSolrToReloadCore(int nPort, String sCore) throws Exception {
+		final String sReloadCommand = "http://localhost:" + nPort + "/solr/admin/cores?action=RELOAD&core=";
 		HttpURLConnection uriConn = (HttpURLConnection)new URL(sReloadCommand + sCore).openConnection();
     	InputStreamReader in = new InputStreamReader(uriConn.getInputStream(), "UTF8");
     	StringBuffer sb = new StringBuffer();
@@ -29,7 +28,11 @@ public class SolrCoreReloader extends edu.usu.cosl.util.Daemon {
 		try {
 			for (Enumeration<String> eCores = vNames.elements(); eCores.hasMoreElements();)
 			{
-				askSolrToReloadCore(eCores.nextElement());
+				try {
+					askSolrToReloadCore(8982, eCores.nextElement());
+				}catch (Exception e2) {
+					askSolrToReloadCore(8983, eCores.nextElement());
+				}
 			}
 			logger.debug("Solr has reloaded its cores.");
 		}catch(Exception e) {
